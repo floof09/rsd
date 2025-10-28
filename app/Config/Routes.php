ef64c01d18338f4e5136c542517c70c16a125407
floof09 <fapflores@gmail.com>
2025-10-28 15:35:56 +0800

    Update default route to redirect to Auth::login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::login');
 
 // Auth Routes
 $routes->get('auth/login', 'Auth::login');
